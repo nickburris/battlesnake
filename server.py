dc159d911e3d471dc4a11dcae12363e0d1977393
Nick Burris <nickburris13@gmail.com>
2020-09-07 02:53:34 -0400

    Choose the most breathing room

--- server.py
+++ server.py
@@ -1,5 +1,6 @@
 import os
 import random
+import copy
 
 import cherrypy
 
@@ -8,10 +9,27 @@ This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
 
+def in_board_range(board, x, y):
+  if (x < 0 or x >= len(board) or
+      y < 0 or y >= len(board[0])):
+    return False
+  return True
+
+def board_space_unoccupied(board, x, y):
+  if not in_board_range(board, x, y):
+    return False
+  
+  # Check immediate collision
+  if board[x][y] in {"body", "head"}:
+    return False
+
+  return True
+
 class Battlesnake(object):
     # State set per-turn
     data = None
     board = None
+    breathing_rooms = {}
 
     # State stored across turns
     just_ate = False
@@ -20,7 +38,7 @@ class Battlesnake(object):
       self.just_ate = False
 
     def make_board(self):
-      self.board = [[None for y in range(self.data["board"]["height"])]
+      self.board = [["space" for y in range(self.data["board"]["height"])]
           for x in range(self.data["board"]["width"])]
       
       for snake in self.data["board"]["snakes"]:
@@ -44,23 +62,23 @@ class Battlesnake(object):
         dest_x+=1;
       
       return dest_x, dest_y
-    
+
+    def in_range(self, x, y):
+      return in_board_range(self.board, x, y)
+
+    def unoccupied(self, x, y):
+      return board_space_unoccupied(self.board, x, y)
+
     def possible(self, m):
       dest_x, dest_y = self.get_dest(m)
-      
-      # Check bounds
-      if (dest_x < 0 or dest_x >= self.data["board"]["width"] or
-          dest_y < 0 or dest_y >= self.data["board"]["height"]):
-        return False
-      
-      # Check immediate collision
-      if self.board[dest_x][dest_y] in {"body", "head"}:
-        return False
-      
-      return True
+      return self.unoccupied(dest_x, dest_y)
 
     def will_eat(self, m):
       dest_x, dest_y = self.get_dest(m)
+
+      if not self.in_range(dest_x, dest_y):
+        return False
+
       if self.board[dest_x][dest_y] == "food":
         return True
       return False
@@ -94,9 +112,42 @@ class Battlesnake(object):
     def growing(self):
       return self.just_ate
 
+    # Returns the area of the shape including the space in direction m
+    def breathing_room(self, m):
+      if m in self.breathing_rooms:
+        return self.breathing_rooms[m]
+
+      dest_x, dest_y = self.get_dest(m)
+      # Dest is expected to be unoccupied
+      if not self.unoccupied(dest_x, dest_y):
+        return 0
+
+      board = copy.deepcopy(self.board)
+      room = 0
+      queue = []
+      queue.append((dest_x, dest_y))
+      while queue:
+        (x, y) = queue.pop(0)
+        if not board_space_unoccupied(board, x, y):
+          continue
+        if board[x][y] == "mark":
+          continue
+        room += 1
+        board[x][y] = "mark"
+
+        moves = [
+          (dx,dy) for (dx,dy) in [(x+1,y),(x,y+1),(x-1,y),(x,y-1)] if self.in_range(dx, dy)]
+        for (dx, dy) in moves:
+          if board_space_unoccupied(board, dx, dy):
+            queue.append((dx, dy))
+
+      self.breathing_rooms[m] = room
+      return room
+
     def move(self, data):
         self.data = data
         self.make_board()
+        self.breathing_rooms = {}
 
         move = "up"
 
@@ -109,14 +160,22 @@ class Battlesnake(object):
           print(f"MOVE: {to_tail} (following tail)")
           return {"move": to_tail}
 
-        # Constricted moves probably lead to death
-        # TODO remove constricted moves, like turning toward a corner/cave
-        # ... follow up TODO what if they're all constricted? Then
-        # constricted moves might be necessary and some constricted moves
-        # may not be killers (e.g. they open up before it's too late)
-
-        # Choose a random possible direction to move in
+        # Prefer to move in the direction with the most "breathing room"
+        # TODO consider that a areas may open up "before it's too late",
+        # i.e. if we're choosing between an area of 4 and an area of 6 but the 
+        # area of 4 would expand to 20 before it's too late then we'd want to go
+        # that way. Similarly, the area with the most breathing room might close
+        # off.
+        possible_moves.sort(key=self.breathing_room, reverse=True)
         if possible_moves:
+          most_room = self.breathing_room(possible_moves[0])
+        preferred_moves = [
+            m for m in possible_moves if self.breathing_room(m) == most_room]
+
+        # Choose a random direction to move in
+        if preferred_moves:
+          move = random.choice(preferred_moves)
+        elif possible_moves:
           move = random.choice(possible_moves)
         else:
           print("No possible moves!")
