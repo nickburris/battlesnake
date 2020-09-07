c8fb1bf3f3546fffcee44e113ca464f9a5d6fcee
Nick Burris <nickburris13@gmail.com>
2020-09-07 01:01:35 -0400

    Create Server class

--- server.py
+++ server.py
@@ -8,47 +8,16 @@ This is a simple Battlesnake server written in Python.
 For instructions see https://github.com/BattlesnakeOfficial/starter-snake-python/README.md
 """
 
-
 class Battlesnake(object):
     # State set per-turn
     data = None
     board = None
 
     # State stored across turns
-    # TODO this assumes one game at a time, move all logic to a different
-    # class and create an instance per game ID
     just_ate = False
 
-    @cherrypy.expose
-    @cherrypy.tools.json_out()
-    def index(self):
-        # This function is called when you register your Battlesnake on play.battlesnake.com
-        # It controls your Battlesnake appearance and author permissions.
-        # TIP: If you open your Battlesnake URL in browser you should see this data
-        return {
-            "apiversion": "1",
-            "author": "nickburris",
-            "color": "#fc6149",
-            "head": "pixel",
-            "tail": "hook",
-        }
-
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    def start(self):
-        # This function is called everytime your snake is entered into a game.
-        # cherrypy.request.json contains information about the game that's about to be played.
-        # TODO: Use this function to decide how your snake is going to look on the board.
-        data = cherrypy.request.json
-
-        # Initialize game state
-        # TODO assumes one game at a time, should actually create a new
-        # game instance.
-        self.growing = False
-
-        print("START")
-        print(data)
-        return "ok"
+    def __init__(self):
+      self.just_ate = False
 
     def make_board(self):
       self.board = [[None for y in range(self.data["board"]["height"])]
@@ -125,13 +94,8 @@ class Battlesnake(object):
     def growing(self):
       return self.just_ate
 
-    @cherrypy.expose
-    @cherrypy.tools.json_in()
-    @cherrypy.tools.json_out()
-    def move(self):
-        # This function is called on every turn of a game. It's how your snake decides where to move.
-        # Valid moves are "up", "down", "left", or "right".
-        self.data = cherrypy.request.json
+    def move(self, data):
+        self.data = data
         self.make_board()
 
         move = "up"
@@ -160,8 +124,51 @@ class Battlesnake(object):
         # Set whether we'll be growing next move
         self.just_ate = self.will_eat(move)
 
-        print(f"MOVE: {move}")
-        return {"move": move}
+        print(f"Move {move}")
+        return move
+
+    def end(self, data):
+        # TODO determine if we won
+        print("GG")
+
+class Server(object):
+    games = {}
+
+    @cherrypy.expose
+    @cherrypy.tools.json_out()
+    def index(self):
+        # This function is called when you register your Battlesnake on play.battlesnake.com
+        # It controls your Battlesnake appearance and author permissions.
+        # TIP: If you open your Battlesnake URL in browser you should see this data
+        return {
+            "apiversion": "1",
+            "author": "nickburris",
+            "color": "#fc6149",
+            "head": "pixel",
+            "tail": "hook",
+        }
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    def start(self):
+        # This function is called everytime your snake is entered into a game.
+        # cherrypy.request.json contains information about the game that's about to be played.
+        data = cherrypy.request.json
+        id = data["game"]["id"]
+        print(f"Starting game {id}")
+        self.games[id] = Battlesnake()
+        return "ok"
+
+    @cherrypy.expose
+    @cherrypy.tools.json_in()
+    @cherrypy.tools.json_out()
+    def move(self):
+        # This function is called on every turn of a game. It's how your snake decides where to move.
+        # Valid moves are "up", "down", "left", or "right".
+        data = cherrypy.request.json
+        id = data["game"]["id"]
+        print(f"Game {id}")
+        return {"move": self.games[id].move(data)}
 
     @cherrypy.expose
     @cherrypy.tools.json_in()
@@ -169,13 +176,15 @@ class Battlesnake(object):
         # This function is called when a game your snake was in ends.
         # It's purely for informational purposes, you don't have to make any decisions here.
         data = cherrypy.request.json
-
-        print("END")
+        id = data["game"]["id"]
+        print(f"Game {id} over")
+        self.games[id].end(data)
+        self.games[id] = None
         return "ok"
 
 
 if __name__ == "__main__":
-    server = Battlesnake()
+    server = Server()
     cherrypy.config.update({"server.socket_host": "0.0.0.0"})
     cherrypy.config.update(
         {"server.socket_port": int(os.environ.get("PORT", "8080")),}
